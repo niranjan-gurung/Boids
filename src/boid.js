9cd542ac6e8c26d330371c316f04dd014c460b8e
niranjan-gurung <gurung5130@gmail.com>
2023-06-19 11:51:39 +0100

    reimplementation with position, velocity, and acceleration - refactoring...

--- src/boid.js
+++ src/boid.js
@@ -4,8 +4,7 @@ import { canvasWidth, canvasHeight } from './app.js';
 export default class Boid {
   // setup each point of triangle
   constructor() {
-    this.generateInitialBoidPosition();
-    this.speed = 1.5;
+    this.speed = 1.2;
     this.width = 5,
     this.height = 7;
     this.perceptionRadius = 100;    // boid view angle radius
@@ -13,69 +12,52 @@ export default class Boid {
     this.isInsideArc = false;
     this.turnAngle = 0.0;
     this.target = false;
-  }
 
-  // generate random starting positions for boids,
-  // will be outside of the canvas, and will all be angled differently:
-  generateInitialBoidPosition() {
+    // boid will be facing random direction: 
     this.angle = this.getRandomNumber(-45, 315);
     this.radians = this.toRadians(this.angle);
 
-    // pos = center of canvas: 
-    this.x = this.getRandomNumber(0, canvasWidth);
-    this.y = this.getRandomNumber(0, canvasHeight);
-  }
+    this.maxForce = 5;
 
-  // return random angle between -45 and 315:
-  getRandomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+    // generate random starting positions for boids:
+    this.position = { 
+      x: this.getRandomNumber(0, canvasWidth),
+      y: this.getRandomNumber(0, canvasHeight),
+    };
 
-  // degree -> radians conversion:
-  toRadians(degree) {
-    return degree * Math.PI / 180;
+    // set velocity of each boid based on its direction:
+    this.velocity = { 
+      x: Math.cos(this.radians) * this.speed,
+      y: Math.sin(this.radians) * this.speed 
+    };
+    
+    this.acceleration = { x: 0, y: 0 };
   }
 
   flock(boids) {
     const limitTurnForce = 1.1;
     
-    let separation = this.separation(boids);
-    this.turnAngle = Math.atan2(separation.y, separation.x);
-    this.radians += this.toRadians(this.turnAngle);
+    // let separation = this.separation(boids);
+    // this.turnAngle = Math.atan2(separation.y, separation.x);
+    // this.radians += this.toRadians(this.turnAngle);
 
     let alignment = this.alignment(boids);
-    this.turnAngle = Math.atan2(alignment.y, alignment.x);
-    this.turnAngle /= limitTurnForce;
-    this.radians += this.toRadians(this.turnAngle);
+    this.acceleration = alignment;
+    //this.turnAngle = Math.atan2(alignment.y, alignment.x);
+    //this.turnAngle /= limitTurnForce;
+    //this.radians += this.toRadians(this.acceleration);
     
-    let cohesion = this.cohesion(boids);
-    this.turnAngle = Math.atan2(cohesion.y, cohesion.x);
-    this.turnAngle /= limitTurnForce;
-    this.radians += this.toRadians(this.turnAngle);
+    // let cohesion = this.cohesion(boids);
+    // this.turnAngle = Math.atan2(cohesion.y, cohesion.x);
+    // this.turnAngle /= limitTurnForce;
+    // this.radians += this.toRadians(this.turnAngle);
   }
 
   alignment(boids) {
-    const currentBoidRotation = this.radians;
-    // get main boid's current direction/velocity:
-    // vecB:
-    let currentBoidDir = {
-      x: Math.cos(currentBoidRotation) * this.speed, 
-      y: Math.sin(currentBoidRotation) * this.speed,
-    };
-
     let steering = { x: 0, y: 0 };
     let total = 0;
 
     for (let other of boids) {
-      const otherBoidRotation = other.radians;
-      // get other boid's current direction/velocity:
-      // vecB:
-      let otherBoidDir = {
-        x: Math.cos(otherBoidRotation) * this.speed, 
-        y: Math.sin(otherBoidRotation) * this.speed,
-      };
       // don't compare ourselves - skip current loop if so:
       if (other != this) {
         // vecA:
@@ -83,17 +65,17 @@ export default class Boid {
         // - also gives us direction pointing from 'us' to the 'other' boid.
         //    - this is used to turn 'us' in opposite direction to 'other' boid.
         let diff = {
-          dx: this.x - other.x,
-          dy: this.y - other.y,
+          x: this.position.x - other.position.x,
+          y: this.position.y - other.position.y,
         };
         // distance between 2 boids:
-        const dst = Math.sqrt(diff.dx*diff.dx + diff.dy*diff.dy);
+        const dst = Math.hypot(diff.x, diff.y);
         
         // normalise vecA:
         normalise(diff, dst);
         
         // dot product between vecA and vecB:
-        let dp = dot(diff, currentBoidDir);
+        let dp = dot(diff, this.velocity);
   
         // check if boid is within other's radius:
         let isInsideRadius = dst < this.perceptionRadius;
@@ -103,8 +85,7 @@ export default class Boid {
   
         // only true if boid is within other's radius AND perception arc:
         if (this.isInsideArc) {
-          steering.x += otherBoidDir.x;
-          steering.y += otherBoidDir.y;
+          this.add2DVec(steering, other.velocity);
           total++;
         }
       }
@@ -113,13 +94,18 @@ export default class Boid {
     }
 
     if (total > 0) {
-      steering.x /= total;
-      steering.y /= total;
+      normalise(steering, total);
+      this.sub2DVec(steering, this.velocity);
+      let hypot = Math.hypot(steering.x, steering.y);
+      this.clamp(hypot, 0, this.maxForce);
 
-      steering.x -= currentBoidDir.x;
-      steering.y -= currentBoidDir.y;
+      console.log(hypot);
     }
     return steering;
+  }
+
+  clamp(num, min, max) {
+    return Math.max(min, Math.min(num, max));
   }
 
   cohesion(boids) {
@@ -142,8 +128,8 @@ export default class Boid {
         // - also gives us direction pointing from 'us' to the 'other' boid.
         //    - this is used to turn 'us' in opposite direction to 'other' boid.
         let diff = {
-          dx: this.x - other.x,
-          dy: this.y - other.y,
+          dx: this.position.x - other.position.x,
+          dy: this.position.y - other.position.y,
         };
         // distance between 2 boids:
         const dst = Math.sqrt(diff.dx*diff.dx + diff.dy*diff.dy);
@@ -201,8 +187,8 @@ export default class Boid {
         // - also gives us direction pointing from 'us' to the 'other' boid.
         //    - this is used to turn 'us' in opposite direction to 'other' boid.
         let diff = {
-          dx: this.x - other.x,
-          dy: this.y - other.y,
+          dx: this.position.x - other.position.x,
+          dy: this.position.y - other.position.y,
         };
         // distance between 2 boids:
         const dst = Math.sqrt(diff.dx*diff.dx + diff.dy*diff.dy);
@@ -241,22 +227,28 @@ export default class Boid {
   }
 
   update() {
-    this.x += Math.cos(this.radians) * this.speed;
-    this.y += Math.sin(this.radians) * this.speed;
+    // this.x += Math.cos(this.radians) * this.speed;
+    // this.y += Math.sin(this.radians) * this.speed;
+
+    // update position:
+    this.add2DVec(this.position, this.velocity);
+
+    // update velocity:
+    this.add2DVec(this.velocity, this.acceleration);
 
     // screen wrapping:
     // if boid goes off screen, wrap its position to the other side:
     // horizontal wrap:
-    if      (this.x + this.width < 0)           this.x = canvasWidth + this.width;
-    else if (this.x - this.width > canvasWidth) this.x = 0 - this.width;
+    if      (this.position.x + this.width < 0)           this.position.x = canvasWidth + this.width;
+    else if (this.position.x - this.width > canvasWidth) this.position.x = 0 - this.width;
     // vertical wrap:
-    if      (this.y + this.height < 0)            this.y = canvasHeight + this.height;
-    else if (this.y - this.height > canvasHeight) this.y = 0 - this.height;
+    if      (this.position.y + this.height < 0)            this.position.y = canvasHeight + this.height;
+    else if (this.position.y - this.height > canvasHeight) this.position.y = 0 - this.height;
   }
     
   draw(ctx) {
     ctx.save();
-    ctx.translate(this.x, this.y);    // realign origin to center of current boid.
+    ctx.translate(this.position.x, this.position.y);    // realign origin to center of current boid.
     ctx.rotate(this.radians);
 
     //boid's view angle:
@@ -281,13 +273,36 @@ export default class Boid {
     ctx.fill();
     ctx.restore();
   }
+
+  /*** helper methods: ***/
+  add2DVec(vec1, vec2) {
+    vec1.x += vec2.x; 
+    vec1.y += vec2.y;
+  }
+
+  sub2DVec(vec1, vec2) {
+    vec1.x -= vec2.x; 
+    vec1.y -= vec2.y;
+  }
+
+  // return random angle between -45 and 315:
+  getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  // degree -> radians conversion:
+  toRadians(degree) {
+    return degree * Math.PI / 180;
+  }
 };
 
 // utility functions:
 function normalise(vec, dst) {
-  vec.dx /= dst, vec.dy /= dst;
+  vec.x /= dst, vec.y /= dst;
 }
 
 function dot(vec1, vec2) {
-  return vec1.dx*vec2.x + vec1.dy*vec2.y;
+  return vec1.x*vec2.x + vec1.y*vec2.y;
 }
